49c90ad7090c628f4a5220b61e129bfa9bf72169
Oleh Rukin <3680312+ximura@users.noreply.github.com>
2024-02-17 14:47:32 +0200

    add tonic-reflection

--- build.rs
+++ build.rs
@@ -1,4 +1,13 @@
+use std::{env, path::PathBuf};
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+  let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+  tonic_build::configure()
+      .file_descriptor_set_path(out_dir.join("weather_descriptor.bin"))
+      .compile(&["./proto/weather.proto"], &["proto"])
+      .unwrap();
+  
     tonic_build::compile_protos("./proto/weather.proto")?;
+
     Ok(())
   }
